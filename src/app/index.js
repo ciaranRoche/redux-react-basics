b3620936ca4c15e9d29dd086df7028571e45e05a
ciaranRoche <ciaranroche@hotmail.co.uk>
2017-10-10 19:47:17 +0100

    basic redux actions

--- src/app/index.js
+++ src/app/index.js
@@ -1,31 +1,70 @@
-import React from "react";
-import {render} from "react-dom";
+// import React from "react";
+// import {render} from "react-dom";
 
-import { User } from './components/User';
-import { Main } from './components/Main';
+// import { User } from './components/User';
+// import { Main } from './components/Main';
 
-class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            username: "Max"
-        };
+// class App extends React.Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             username: "Max"
+//         };
+//     }
+
+//     changeUsername(newName) {
+//         this.setState({
+//             username: newName
+//         });
+//     }
+
+//     render() {
+//         return (
+//             <div className="container">
+//                 <Main changeUsername={this.changeUsername.bind(this)}/>
+//                 <User username={this.state.username}/>
+//             </div>
+//         );
+//     }
+// }
+
+// render(<App />, window.document.getElementById('app'));
+
+import { createStore } from 'redux';
+
+//reducer passed to create store
+const reducer = (state, action) => {
+    switch (action.type){
+        case "ADD" : 
+            state = state + action.payload;
+            break;
+        case "SUBTRACT":
+            state = state - action.payload;
+            break;
     }
+    return state;
+};
 
-    changeUsername(newName) {
-        this.setState({
-            username: newName
-        });
-    }
+//store takes in reducer function and a prop
+const store = createStore(reducer, 1);
 
-    render() {
-        return (
-            <div className="container">
-                <Main changeUsername={this.changeUsername.bind(this)}/>
-                <User username={this.state.username}/>
-            </div>
-        );
-    }
-}
+//subscribe triggers when reducer is fired
+store.subscribe(() => {
+    console.log("Store updated!", store.getState());
+});
 
-render(<App />, window.document.getElementById('app'));
+//dispatch function takes js object calls reducer in store
+store.dispatch({
+    type: "ADD",
+    payload: 10
+});
+store.dispatch({
+    type: "ADD",
+    payload: 22
+});
+store.dispatch({
+    type: "SUBTRACT",
+    payload: 22
+});
+
+
